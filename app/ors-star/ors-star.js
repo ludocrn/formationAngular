c8cf24936248af104b707f45994a048da43f1a1f
ludovic <cornu.ludovic@gmail.com>
2017-03-21 17:27:38 +0100

    Stars avec controller dans ors-star.js

--- app/ors-star/ors-star.js
+++ app/ors-star/ors-star.js
@@ -3,38 +3,37 @@
 
 	var app = angular.module('ors-star', []);
 
-	app.directive('orsStar', ['$compile', function($compile) {
-		return {
-			scope: {
-				n: '=?note',
-			},
-			link: function(scope, element, attrs) {
-				console.log('orsStar link', arguments);
-				scope.update = function(n) {
+	app.component('orsStar', {
+		bindings: {
+			n: '=?note',
+		},
+		controller: ['$scope', '$element', '$compile',
+			function($scope, $element, $compile) {
+				console.log('orsStar ctrl', arguments);
+				var ctrl = this;
+				ctrl.update = function(n) {
 					console.log('update', arguments);
-					scope.n = n;
+					ctrl.n = n;
 				};
-				scope.$watch('n', function() {
+				$scope.$watch('$ctrl.n', function() {
 					var html = '';
 					var note = 4;
-					note = (scope.n === undefined) ? note : Number(scope.n);
+					note = (ctrl.n === undefined) ? note : Number(ctrl.n);
 					note = (isNaN(note)) ? 0 : note;
 					note = (note > 5) ? 5 : note;
 					note = (note < 0) ? 0 : note;
 					for (let i = 0; i < note; i++) {
-						html += '<img ng-click="update(' + (i + 1) + ')" src="./ors-star/img/yellow_star.png" />';
+						html += '<img ng-click="$ctrl.update(' + (i + 1) + ')" src="./ors-star/img/yellow_star.png" />';
 					}
 
 					for (let i = note; i < 5; i++) {
-						html += '<img ng-click="update(' + (i + 1) + ')" src="./ors-star/img/white_star.png" />';
+						html += '<img ng-click="$ctrl.update(' + (i + 1) + ')" src="./ors-star/img/white_star.png" />';
 					}
-					element.html(html);
-					$compile(element.contents())(scope);
+					$element.html(html);
+					$compile($element.contents())($scope);
 				});
-
-
 			}
-		};
-	}]);
+		]
+	});
 
 })();
