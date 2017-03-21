581d1cbc457b6d5d590e0c69c427342d44703555
ludovic <cornu.ludovic@gmail.com>
2017-03-21 13:01:35 +0100

    initialisation projet

---
+++ app/app.js
@@ -0,0 +1,55 @@
+(function() {
+	'use strict';
+
+	var app = angular.module('main', ['ors-route']);
+
+	app.run(['$rootScope', '$location', function($rootScope, $location) {
+		$rootScope.isActive = function(url) {
+			var path = $location.path();
+			console.log('path', path);
+			return url === path;
+		};
+		console.log('run');
+	}]);
+
+	app.directive('orsActive', function() {
+		return {
+			restrict: 'A',
+			controller: ['$scope', '$attrs', '$element', '$location',
+				function($scope, $attrs, $element, $location) {
+					console.log('$attrs', $attrs);
+					$scope.$on('$routeChangeStart', function(next, current) {
+						var url = $attrs.href;
+						var path = '#' + $location.path();
+						if (url === path) {
+							$element.addClass('active');
+						} else {
+							$element.removeClass('active');
+						}
+					});
+				}
+			]
+		};
+	});
+
+	app.component('orsHeader', {
+		templateUrl: 'tmpl/ors-header.html'
+	});
+
+
+	app.directive('orsBody', function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'tmpl/ors-body.html'
+		};
+	});
+
+
+	app.directive('orsFooter', function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'tmpl/ors-footer.html',
+		};
+	});
+
+})();
---
+++ app/ors-route/ors-route.js
@@ -0,0 +1,29 @@
+(function() {
+	'use strict';
+
+	var app = angular.module('ors-route', ['ngRoute']);
+
+	app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+		$locationProvider
+			.html5Mode(false)
+			.hashPrefix('');
+
+		$routeProvider
+			.when('/', {
+				templateUrl: 'ors-route/tmpl/home.html'
+			})
+			.when('/product', {
+				templateUrl: 'ors-route/tmpl/product.html'
+			})
+			.when('/service', {
+				templateUrl: 'ors-route/tmpl/service.html'
+			})
+			.when('/contact', {
+				templateUrl: 'ors-route/tmpl/contact.html'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+	}]);
+
+})();
